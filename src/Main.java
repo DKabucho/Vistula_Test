3ec75bde68b1ac2c04c6cc9c41bb6325bc015c8e
DKabucho <danielkabucho2004@gmail.com>
2025-02-22 12:15:45 +0100

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
-
-
+        System.out.println("Welcome to Vistula!");
 
     }
 }
